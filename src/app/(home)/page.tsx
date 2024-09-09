64b8db599bed46560765f7524073819e64036b1c
MOHAMED <mohamed1239455@gmail.com>
2024-09-09 10:43:53 +0200

    add tailwind class sort

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,36 +1,36 @@
-import Search from '@/components/Search';
-import { Button } from '@/components/ui/button';
-import OurDeals from '@/components/OurDeals';
+import Search from "@/components/Search";
+import { Button } from "@/components/ui/button";
+import OurDeals from "@/components/OurDeals";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col items-center">
         <h1 className="text-7xl">Doctorii</h1>
-        <h2 className="text-sm mt-4 font-sans">Appointment Made Easier</h2>
+        <h2 className="mt-4 font-sans text-sm">Appointment Made Easier</h2>
       </div>
-      <div className="flex flex-col items-center gap-8 mt-16">
+      <div className="mt-16 flex flex-col items-center gap-8">
         <h1 className="text-3xl">Book a Doctor</h1>
         <Search />
 
-        <Button className="rounded-full px-24 py-8 bg-blue-400 text-xl">
+        <Button className="rounded-full bg-blue-400 px-24 py-8 text-xl">
           Search
         </Button>
 
-        <p className="font-sans text-center mt-12">
+        <p className="mt-12 text-center font-sans">
           <span className="text-blue-400">Doctorii</span>
            is your one-stop online platform for convenient and affordable
           healthcare. We connect <br /> you with qualified doctors from the
-          comfort of your home. Whether you need a routine <br />{' '}
+          comfort of your home. Whether you need a routine <br />{" "}
           checkup, specialist consultation, or urgent medical advice.
         </p>
       </div>
       {/* OUR DOCTORS */}
-      <div className="bg-zinc-800 p-6 flex justify-center gap-8 mt-24">
-        <Button className="bg-zinc-50 px-10 text-zinc-800 rounded-full">
+      <div className="mt-24 flex justify-center gap-8 bg-zinc-800 p-6">
+        <Button className="rounded-full bg-zinc-50 px-10 text-zinc-800">
           Our Doctors
         </Button>
-        <Button className="bg-zinc-50 px-10 text-zinc-800 rounded-full">
+        <Button className="rounded-full bg-zinc-50 px-10 text-zinc-800">
           Our Deals
         </Button>
       </div>
--- src/components/OurDeals.tsx
+++ src/components/OurDeals.tsx
@@ -1,9 +1,9 @@
-import ImagesSlider from '@/components/ui/ImagesSlider';
-import React from 'react';
+import ImagesSlider from "@/components/ui/ImagesSlider";
+import React from "react";
 
 const CardDetails = {
-  image: '/dental.png',
-  title: 'Teeth Cleaning',
+  image: "/dental.png",
+  title: "Teeth Cleaning",
   oldPrice: 300,
   newPrice: 150,
   discount: 50,
@@ -12,7 +12,7 @@ const OurDeals = () => {
   return (
     <section className="p-12">
       <h2 className="text-5xl">Our Deals</h2>
-      <div className="bg-zinc-300 p-12 mx-24 rounded-md mt-8">
+      <div className="mx-24 mt-8 rounded-md bg-zinc-300 p-12">
         <ImagesSlider />
       </div>
     </section>
